16c8346ffab5a9b11fb3de3b2235a700a799e52a
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-31 17:30:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,8 @@ def monopoly_with_fourth_tier
    base_hash = {
   :railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned =>50, :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {} }}
   
-   monopoly_with_third_tier[:railraods][:names][:reading_railroad][:mortgage_value] = 100
 }
 
 end
+
+monopoly_with_third_tier[:railraods][:names][:reading_railroad][:mortgage_value] = 100
